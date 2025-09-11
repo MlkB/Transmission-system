bd9318be6ad2eb9a2b2d42bc92cc524db0a9c08f
elie_rbr <liliphilippine2001@gmail.com>
2025-09-11 15:17:08 +0200

    Update SourceFixe.java

--- src/sources/SourceFixe.java
+++ src/sources/SourceFixe.java
@@ -8,26 +8,19 @@ public class SourceFixe extends Source {
 		super();
 	}
 	
-	public void genererInformation(String message) {
+	public void generer(String message) {
 		Information<Boolean> informationBinaire = new Information<Boolean>();
         for (int j = 0; j < message.length(); j++) {
         	if (message.charAt(j) =='1') informationBinaire.add(true);
         	else if (message.charAt(j) =='0') informationBinaire.add(false);
         	else {
-        		System.out.println("aie aie aie");
         		break;
         	}
         }
         
         this.informationGeneree = informationBinaire;
 	}
-	
-	public static void main(String[] args) {
-		SourceFixe S1 = new SourceFixe();
-		S1.genererInformation("111");
-		System.out.println(S1.informationGeneree);
-	}
-	
+
 }
 
 
