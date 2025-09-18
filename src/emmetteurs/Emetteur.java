c349aaedec2d88a5fd02bcc47e871444f061a4dd
elie-rbr <liliphilippine2001@gmail.com>
2025-09-18 22:09:46 +0200

    ajout du début de la javadoc

--- src/emmetteurs/Emetteur.java
+++ src/emmetteurs/Emetteur.java
@@ -5,6 +5,12 @@ import information.Information;
 import information.InformationNonConformeException;
 import sources.Source;
 
+/**
+ * Classe Emetteur représentant un objet de la chaîne de transmission
+ * qui reçoit l'information booléenne de la source et la transforme
+ * en information analogique
+ * @param <T>
+ */
 public class Emetteur<T> extends Source<Float> implements  DestinationInterface <T>{        
     
     private Information<Boolean> informationRecue;
@@ -19,18 +25,24 @@ public class Emetteur<T> extends Source<Float> implements  DestinationInterface 
 
     //Iterator<DestinationInterface<E>> it = destinationsConnectees.iterator();
 
+    /**
+     * Fonction permettant de convertir l'information booléenne reçue
+     * en information analogique selon trois types de codages :
+     * NRZ, RZ et NRZT. Le type de codage est déterminé selon la valeur
+     * de la fonction TypeCodage
+     * @throws InformationNonConformeException
+     */
+    public void convertir_signal() throws InformationNonConformeException {
 
-        public void convertir_signal() throws InformationNonConformeException {
-                    
             informationGeneree = new Information<>();
-            // codage en ligne 
-        
+            // codage en ligne
+
             if ("NRZ".equalsIgnoreCase(TypeCodage)) {
                  for (Boolean bit : informationRecue) {
                     informationGeneree.add(bit ? 1.0f : -1.0f);
                 }
-            
-            
+
+
             } else if ("RZ".equalsIgnoreCase(TypeCodage)) {
                 for (Boolean bit : informationRecue) {
                     informationGeneree.add(bit ? 1.0f : -1.0f);
@@ -56,22 +68,32 @@ public class Emetteur<T> extends Source<Float> implements  DestinationInterface 
                 throw new InformationNonConformeException("Type de codage inconnu");
             }
         }
-          @Override
-        public void recevoir(Information<T> information) throws InformationNonConformeException {
+
+    /**
+     * Fonction permettant à l'émetteur de récupérer l'information envoyée par la source.
+     * @param information  l'information  à recevoir
+     * @throws InformationNonConformeException si l'information est nulle
+     */
+    @Override
+    public void recevoir(Information<T> information) throws InformationNonConformeException {
             if (information == null) {
                 throw new InformationNonConformeException("L'information est vide");
             }
             this.informationRecue = (Information<Boolean>) information;
             convertir_signal();
             emettre();
-        
+
         }
 
-          @Override
-          public Information getInformationRecue() {
-                return this.informationRecue;
-          }
-          
+    /**
+     * Fonction permettant de récupérer l'information reçue par l'émetteur
+     * @return informationRecue
+     */
+    @Override
+    public Information getInformationRecue() {
+            return this.informationRecue;
+      }
+
 
 
     }
--- src/sources/SourceFixe.java
+++ src/sources/SourceFixe.java
@@ -2,12 +2,21 @@ package sources;
 
 import information.Information;
 
+/**
+ * Classe d'un composant représentant la source fixe d'une chaîne de transmission
+ */
 public class SourceFixe extends Source {
 
 	public SourceFixe() {
 		super();
 	}
-	
+
+	/**
+	 * permet de générer un message booléen
+	 * @param message
+	 * le paramètre message doit être sous la forme d'une chaîne de caractères représentant un message
+	 * binaire
+	 */
 	public void generer(String message) {
 		Information<Boolean> informationBinaire = new Information<Boolean>();
         for (int j = 0; j < message.length(); j++) {
